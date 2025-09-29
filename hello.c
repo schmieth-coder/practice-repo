4c19beacde01fb90f17bc31c7e275b8e8ebc31e2
schmieth-coder <schmieth@gmail.com>
2025-09-29 13:18:34 -0500

    update to hello.c file with comments

--- hello.c
+++ hello.c
@@ -1,6 +1,10 @@
+// Include the standard input/output library for printf function
 #include <stdio.h>
 
+// Main function - entry point of the program
 int main() {
+    // Print the message to the console, \n creates a new line
     printf("Hello, World! This is my second commit\n");
+    // Return 0 to indicate successful program execution
     return 0;
 }
